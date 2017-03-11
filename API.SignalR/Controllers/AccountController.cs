dc72d92a43fae930c0d36e1936c6e72356571913
nucleotidz <ahmar.husain1@gmail.com>
2017-03-12 00:47:17 +0530

    OK

---
+++ API.SignalR/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.SignalR.Controllers
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        [HttpGet]
+        [Route("Get")]
+        public HttpResponseMessage Get()
+        {
+          return  Request.CreateResponse(HttpStatusCode.OK, "Ahmar");
+        }
+    }
+}
